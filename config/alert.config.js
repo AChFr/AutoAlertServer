f81de049d734945aace60aaca15f3843b4049ba3
AChFr <chirkovarseni@gmail.com>
2022-04-05 03:33:07 +0200

    without cron

--- config/alert.config.js
+++ config/alert.config.js
@@ -5,7 +5,8 @@ const Vehicle = require("../models/Vehicle.model")
 const User = require("../models/User.model")
 const transporter = require("./transporter.config")
 
-let counter = 0
+
+
 
 
 const mailUser = (address) => {
@@ -13,20 +14,54 @@ const mailUser = (address) => {
         from: "imdbprojectteam@gmail.com",
         to: address,
         subject: `Alerta!!!`,
-        text: `esto es una alerta!! y el contador es ===>>> ${counter} y la hora es ${new Date
-            } `,
-        html: "<p>" + `esto es una alerta!!  y el contador es ===>>> ${counter} y la hora es ${new Date
-            }` + "</p>"
+        text: `esto es una alerta!! y el contador es ===>>> y la hora es ${new Date} `,
+        html: "<p>" + `esto es una alerta!!  y el contador es ===>>> y la hora es ${new Date}` + "</p>"
     })
 }
 
 
 
-var job = new CronJob('*/10 * * * *', () => {
-    let mail = "troysoviet@gmail.com"
+// let launchTime = new Date()
+
+// setInterval(() => {
+//     let currentTime = new Date()
+//     let count = +currentTime - +launchTime
+//     console.log(count)
+// }, 1000)
+
+
+let minutes = 0
+let counter = 0
+
+console.log(" l a u n c h  t i m e " + new Date())
+console.log(" m i n u t e s " + minutes)
+console.log(" c o u n t e r " + counter)
+setInterval(() => {
     counter++
-    mailUser(mail)
+    if (counter === 60) {
+        minutes++
+        console.log(" m i n u t e s " + minutes)
+        counter = 0
+    }
 
-}, null, true, 'America/Los_Angeles')
+    if (minutes >= 30) {
+        mailUser("amorosoperezoso@gmail.com")
+        console.log(" m a i l e d   a t  " + new Date())
+        minutes = 0
+        console.log("now is " + new Date())
+    }
+}, 1000)
 
-job.start();
+
+//the proper way to do it is this, but since heroku has its own "CRON", you have to pay extra to be able to do this. 
+// so I did it the "ugly" way. but its fee!!! ¯\_(ツ)_/¯
+
+
+// var job = new CronJob('*/10 * * * *', () => {
+//     let mail = "troysoviet@gmail.com"
+//     counter++
+//     mailUser(mail)
+
+// }, null, true, 'America/Los_Angeles')
+
+// job.start();
